88fecc7f0c2c8434e24f2d3f2a97a4ec7bbac43e
Praveen Prasad <praveenprasad@Praveens-MacBook-Pro.local>
2016-12-19 12:39:09 +0530

    Create Proxy server, closes #1

---
+++ src/base/xhr.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+let PROXY_URL = null;
+
+export default function xhr(options){
+  let config = {};
+  let useProxy = !!options.useProxy;
+  let proxyUrl = options.useProxy;
+  delete options.useProxy;
+  if(useProxy){
+    let url = typeof(proxyUrl)==='string' ? proxyUrl : PROXY_URL;
+    config = {
+      url:url,
+      data:options,
+      method:'POST'
+    }
+  }else{
+    config = options;
+  }
+
+  return axios(config);
+}
--- src/web/index.js
+++ src/web/index.js
@@ -1,12 +1,16 @@
-import axios from 'axios';
 import init from './database/init';
+import xhr from '../base/xhr.js';
 
 var platform = {
   getKey:function(){
     return "web";
   },
 
-  xhr: axios,
+  xhr: xhr,
+
+  setXHRProxyUrl: function(proxyUrl){
+    PROXY_URL =  proxyUrl;
+  },
 
   dbInit:init
 }
